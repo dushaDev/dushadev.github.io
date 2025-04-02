bf74c2e0ba8f929ca44804c94d45ded810bf3f6e
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-04-02 22:02:44 +0530

    fix: versel url
    VERCEL_URL to NEXT_PUBLIC_VERCEL_URL

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     //     VERCEL_URL: process.env.VERCEL_URL || 'http://localhost:3000',
     //   },
     env: {
-        VERCEL_URL: process.env.VERCEL_URL || 'http://localhost:3000',
+        NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
         RESEND_API_KEY: process.env.RESEND_API_KEY,
       },
     
