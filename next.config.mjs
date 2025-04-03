0d99b7834bf18b8191ddecdae083e564df45e399
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-04-03 09:31:41 +0530

    fix:remove vercel url
    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    // env: {
-    //     // Fallback URL for local development
-    //     VERCEL_URL: process.env.VERCEL_URL || 'http://localhost:3000',
-    //   },
     env: {
-        NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+        // NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
         RESEND_API_KEY: process.env.RESEND_API_KEY,
       },
     
