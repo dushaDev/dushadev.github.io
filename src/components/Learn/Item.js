a37950f22742f894c7b31161edd6e140e1819708
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-07-20 21:42:58 +0530

    Add button item to Learn list and update Item rendering
    Introduced a 'type' property to projectData items in Learn.js, allowing differentiation between 'post' and 'button' types. Updated Item.js to render a special button card when item.type is 'button'.
     Also removed unnecessary padding from Contact.js and added margin-bottom to the Learn section for improved layout.

--- src/components/Learn/Item.js
+++ src/components/Learn/Item.js
@@ -14,43 +14,60 @@ import * as React from "react";
 import Link from "next/link";
 
 export default function Item({ item }) {
-  return (
-    <div className="relative w-72 h-96 border-2 border-primary bg-transparent rounded-sm overflow-hidden group">
-      {/* Added 'group' class to enable hover effects on child elements */}
-      <Link
-        href={item.link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="relative w-full h-48">
-          <img
-            src={item.image}
-            alt={`${item.name} News Image`}
-            className="w-full h-full object-cover"
-          />
-          {/* Date with curved orange background */}
-          <div className="absolute top-0 right-0 px-4 pb-0.5 bg-primary rounded-bl-xl">
-            <span className="text-white text-xs">{item.time}</span>
-          </div>
 
-        </div>
-      </Link>
-      <div className="p-4">
-        <h3 className="text-xl font-semibold text-white mb-2">
-          {item.name}
-        </h3>
-        <p className="text-gray-300 text-sm leading-relaxed overflow-hidden text-ellipsis h-10 mb-4 line-clamp-2"> {/* Added mb-4 for spacing */}
-          {item.description}
-        </p>
-        {/* Read More Button (always visible at the bottom) */}
-        <div className="absolute bottom-4 left-4 right-4 text-center">
+  if (item.type == 'button') {
+    return (
+      <div className="relative w-72 h-96 border-2 border-neutral bg-transparent rounded-sm overflow-hidden group">
+        <div className="absolute inset-0 flex items-center justify-center">
           <Link href={item.link} target="_blank" rel="noopener noreferrer">
             <button className="w-full bg-secondary hover:bg-primary hover:text-secondary transition-colors duration-200 text-primary font-bold py-1 px-2 rounded">
-              Read More
+              Read More Articles
             </button>
           </Link>
         </div>
       </div>
-    </div>
-  );
+    );
+
+  } else {
+    return (
+      <div className="relative w-72 h-96 border-2 border-primary bg-transparent rounded-sm overflow-hidden group">
+        {/* Added 'group' class to enable hover effects on child elements */}
+        <Link
+          href={item.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="relative w-full h-48">
+            <img
+              src={item.image}
+              alt={`${item.name} News Image`}
+              className="w-full h-full object-cover"
+            />
+            {/* Date with curved orange background */}
+            <div className="absolute top-0 right-0 px-4 pb-0.5 bg-primary rounded-bl-xl">
+              <span className="text-white text-xs">{item.time}</span>
+            </div>
+
+          </div>
+        </Link>
+        <div className="p-4">
+          <h3 className="text-xl font-semibold text-white mb-2">
+            {item.name}
+          </h3>
+          <p className="text-gray-300 text-sm leading-relaxed overflow-hidden text-ellipsis h-10 mb-4 line-clamp-2"> {/* Added mb-4 for spacing */}
+            {item.description}
+          </p>
+          {/* Read More Button (always visible at the bottom) */}
+          <div className="absolute bottom-4 left-4 right-4 text-center">
+            <Link href={item.link} target="_blank" rel="noopener noreferrer">
+              <button className="w-full bg-secondary hover:bg-primary hover:text-secondary transition-colors duration-200 text-primary font-bold py-1 px-2 rounded">
+                Read More
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
