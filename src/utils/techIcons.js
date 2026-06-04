f19ee621d6efb23107ec65106b1e0eb888f4eba6
dushan6693 <dushanmadushankabeligala9@gmail.com>
2026-06-04 12:16:21 +0530

    feat: add Arduino and C++ icons to techIcons helper

--- src/utils/techIcons.js
+++ src/utils/techIcons.js
@@ -1,6 +1,6 @@
 import { FaGithub, FaReact, FaJava, FaPython, FaPhp, FaDatabase, FaAndroid, FaFigma, FaBootstrap, FaFlask } from "react-icons/fa";
 import { FaFlutter } from "react-icons/fa6";
-import { SiFirebase, SiFlask } from "react-icons/si";
+import { SiFirebase, SiFlask, SiArduino, SiCplusplus } from "react-icons/si";
 import { RiGeminiFill } from "react-icons/ri";
 import { AiFillOpenAI } from "react-icons/ai";
 import { TbBrandAdobeIllustrator, TbBrandAdobeXd, TbBrandAdobePhotoshop } from "react-icons/tb";
@@ -23,6 +23,8 @@ const mapping = {
   "adobe xd": TbBrandAdobeXd,
   "photoshop": TbBrandAdobePhotoshop,
   "adobe illustrator": TbBrandAdobeIllustrator,
+  "arduino": SiArduino,
+  "c++": SiCplusplus,
 };
 
 export const getTechIcon = (name) => {
